b5b2ac510f1358f5193a286975acef0ede00015d
leodeng <ericdyx@gmail.com>
2016-05-17 00:05:51 -0700

    Add smtp server

--- LeoPiExtIpNotifier.py
+++ LeoPiExtIpNotifier.py
@@ -10,7 +10,7 @@ def get_ext_ip():
     return ext_ip
 
 def read_ext_ip_from_file():
-    file_hdlr = open("externel_ip.txt")
+    file_hdlr = open("external_ip.txt", "r")
     ext_ip = file_hdlr.read()
     file_hdlr.close()
     return ext_ip
@@ -21,37 +21,41 @@ def write_ext_ip_to_file(ext_ip):
     file_hdlr.write(ext_ip)
     file_hdlr.close()
 
-def send_notify_email(ext_ip, from_email, to_email, title):
+def send_notify_email(ext_ip, email, password, title):
     msg = MIMEText(ext_ip)
     msg["Subject"] = title
-    msg["From"] = from_email
-    msg["To"] = to_email
-    server = smtplib.SMTP("localhost")
-    server.sendmail(from_email, to_email, msg.as_string())
+    msg["From"] = email
+    msg["To"] = email
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, msg.as_string())
     server.quit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Leo Raspberry External IP Notifier CLI Parser")
-    parser.add_argument("--from", "-f", action="store", dest="from_email",
-                        help="Email address that sends the notification")
-    parser.add_argument("--to", "-o", action="store", dest="to_email",
-                        help="Email address that should receives the notification")
+    parser.add_argument("--email", "-e", action="store", dest="email",
+                        help="Email address that sends/receive the notification")
+    parser.add_argument("--password", "-p", action="store", dest="password",
+                        help="Password of the email")
     parser.add_argument("--title", "-t", action="store", dest="title",
                         help="Title of notification email")
 
     result = parser.parse_args()
-    from_email = result.from_email
-    to_email = result.to_email
+    email = result.email
+    password = result.password
     title = result.title
 
     ext_ip = get_ext_ip()
-    if not os.path.isfile("external_ip.txt"):
+    if not os.path.isfile("./external_ip.txt"):
         write_ext_ip_to_file(ext_ip)
-        send_notify_email(ext_ip, from_email, to_email, title)
+        send_notify_email(ext_ip, email, password, title)
     else:
         ext_ip = get_ext_ip()
         old_ext_ip = read_ext_ip_from_file()
         if ext_ip != old_ext_ip:
             write_ext_ip_to_file(ext_ip)
-            send_notify_email(ext_ip, from_email, to_email, title)
+            send_notify_email(ext_ip, email, password, title)
 
